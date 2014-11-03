3b3322e12595d81b625aeacdd5a6f2d99b353500
Brett G. Giles <brett.giles@drogar.com>
2014-11-03 09:50:48 -0700

    Add simple thread comparison test
      - code courtesy of Philip Yousef
    
    http://www.restlessprogrammer.com/2013/02/multi-threading-in-jruby.html

---
+++ multi-thread-jruby.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+# Code courtesy of Philip Yousef - http://www.restlessprogrammer.com/2013/02/multi-threading-in-jruby.html
+# used to call java code
+require 'java'
+
+# 'java_import' is used to import java classes
+java_import 'java.util.concurrent.Callable'
+java_import 'java.util.concurrent.FutureTask'
+java_import 'java.util.concurrent.LinkedBlockingQueue'
+java_import 'java.util.concurrent.ThreadPoolExecutor'
+java_import 'java.util.concurrent.TimeUnit'
+
+# Implement a callable class
+class CountMillion
+  include Callable
+  def call
+    count = 0
+    1000000.times do
+      count += 1
+    end
+  end
+end
+
+num_iterations = ARGV[1].to_i #20
+num_threads = ARGV[0].to_i #4
+# Create a thread pool
+executor = ThreadPoolExecutor.new(num_threads, # core_pool_treads
+                                  num_threads, # max_pool_threads
+                                  60, # keep_alive_time
+                                  TimeUnit::SECONDS,
+                                  LinkedBlockingQueue.new)
+
+
+total_time = 0.0
+
+num_iterations.times do |i|
+  tasks = []
+
+  t_0 = Time.now
+  num_threads.times do
+    task = FutureTask.new(CountMillion.new)
+    executor.execute(task)
+    tasks << task
+  end
+
+  # Wait for all threads to complete
+  tasks.each do |t|
+    t.get
+  end
+  t_1 = Time.now
+
+  time_ms = (t_1-t_0) * 1000.0
+  puts "TEST #{i}: Time elapsed = #{time_ms}ms"
+  total_time +=  time_ms
+end
+executor.shutdown()
+
+puts "Average completion time: #{total_time/num_iterations}"
---
+++ multi-thread-ruby.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# Code courtesy of Philip Yousef - http://www.restlessprogrammer.com/2013/02/multi-threading-in-jruby.html
+
+num_iterations = ARGV[1].to_i #20
+num_threads = ARGV[0].to_i #4
+
+# Try counting to 1 million on 4 separate threads 20 times
+total_time = 0.0
+num_iterations.times do |iter|
+  threads = []
+  t_0 = Time.now
+  for i in 1..num_threads
+    threads << Thread.new(i) { |t|
+      count = 0
+      1000000.times do
+        count += 1
+      end
+    }
+  end
+
+  # Wait for all threads to complete
+  threads.each { |thread| thread.join }
+  t_1 = Time.now
+
+  time_ms = (t_1-t_0) * 1000
+  puts "TEST #{iter}: Time elapsed = #{time_ms}ms"
+  total_time +=  time_ms
+end
+
+puts "Average completion time: #{total_time/num_iterations}"
